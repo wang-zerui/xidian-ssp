d28120647bf0f7dde6844629c65223ae9d2fde05
root <root@localhost.localdomain>
2020-11-06 00:25:46 +0800

    commit

--- src/app.config.js
+++ src/app.config.js
@@ -1,7 +1,22 @@
 export default {
   pages: [
-    'pages/index/index'
+    'pages/index/index',
+    "pages/discovery/discovery",
+    "pages/home/home"
+
   ],
+  tabBar: {
+    list: [{
+      pagePath: "pages/index/index",
+      text: "首页"
+    }, {
+      pagePath: "pages/discovery/discovery",
+      text: "发现"
+    }, {
+      pagePath: "pages/home/home",
+      text: "我的"
+    }]
+  },
   window: {
     backgroundTextStyle: 'light',
     navigationBarBackgroundColor: '#fff',
